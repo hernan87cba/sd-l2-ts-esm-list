667542051ac39c93802d2f470e96905354a85b04
Hernan <deciccohernan@gmail.com>
2025-08-22 17:06:46 -0300

    Quito dist de gitignore y subo su contenido a github

---
+++ dist/footer.js
@@ -0,0 +1,8 @@
+export function footerComponent() {
+    const footer = document.createElement("footer");
+    footer.classList.add("footer");
+    footer.innerHTML = "<h1>Footer del listado de precios</h1>";
+    footer.style.border = "solid 3px purple";
+    footer.style.padding = "12px";
+    return footer;
+}
